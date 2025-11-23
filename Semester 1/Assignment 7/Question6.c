f6661824fc33d7d6ab36b1de7cffde51bb0428fb
Mayank Mohanty <mohantymayank07@gmail.com>
2025-11-23 16:34:12 +0530

    Modified program

--- Semester 1/Assignment 7/Question6.c
+++ Semester 1/Assignment 7/Question6.c
@@ -13,9 +13,6 @@ int main()
             spaces++;
         else if((str[i]>=33 && str[i]<=47) || (str[i]>=58 && str[i]<=64) || (str[i]>=91 && str[i]<=96) || (str[i]>=123 && str[i]<=126))
             punc++;
-    }
-    for (i=0;str[i]!='\0';i++)
-    {
         if ((str[i]!=' ') && (i==0 || str[i-1]==' '))
             words++;
     }
